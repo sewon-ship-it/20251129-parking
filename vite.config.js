239d4bb55a295d5296d9665713fee84e919bf8c8
세원 <twinsewon@gamil.com>
2025-12-04 13:38:27 +0900

    chore: vite.config.js 서버 설정 추가

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,11 @@
 import { resolve } from 'path'
 import { defineConfig } from 'vite'
 export default defineConfig({
+    server: {
+        host: '0.0.0.0', // 모든 네트워크 인터페이스에서 접근 가능
+        port: 5173,
+        open: false // 자동으로 브라우저 열지 않음
+    },
     build: {
         rollupOptions: {
             input: {
